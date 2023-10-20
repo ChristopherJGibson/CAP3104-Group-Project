68ce755a2f691097015014bce340a2d03043f8b6
eldeaygo <wplh1997@gmail.com>
2023-10-20 11:08:08 -0400

    here are the cookies for login

--- public/js/index.js
+++ public/js/index.js
@@ -11,3 +11,23 @@ var events = [
 // Parse the array of demo events to load data into the calendar
 // This will be loaded from the server later
 scheduler.parse(events, "json");
+
+// Function to set the cookies
+function setGroupCookies(memberOf, managerOf) {
+   document.cookie = `memberOf=${JSON.stringify(memberOf)}; path=/`;
+   document.cookie = `managerOf=${JSON.stringify(managerOf)}; path=/`;
+}
+
+// Function to get the cookies
+function getGroupCookies() {
+   const cookies = document.cookie.split(';').map(cookie => cookie.trim().split('='));
+   const groupCookies = cookies.filter(cookie => cookie[0] === "memberOf" || cookie[0] === "managerOf");
+
+   const groups = {};
+
+   for (const cookie of groupCookies) {
+       groups[cookie[0]] = JSON.parse(cookie[1]);
+   }
+
+   return groups;
+}
