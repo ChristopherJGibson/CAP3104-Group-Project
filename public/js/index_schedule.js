b7ae073745f27bf882db0902aec28b44942fb766
an372315 <90437547+an372315@users.noreply.github.com>
2023-10-27 12:01:01 -0400

    Just added all the codes to VS

---
+++ public/js/index_schedule.js
@@ -0,0 +1,19 @@
+// Function to set the cookies
+function setGroupCookies(memberOf, managerOf) {
+    document.cookie = `memberOf=${JSON.stringify(memberOf)}; path=/`;
+    document.cookie = `managerOf=${JSON.stringify(managerOf)}; path=/`;
+ }
+ 
+ // Function to get the cookies
+ function getGroupCookies() {
+    const cookies = document.cookie.split(';').map(cookie => cookie.trim().split('='));
+    const groupCookies = cookies.filter(cookie => cookie[0] === "memberOf" || cookie[0] === "managerOf");
+ 
+    const groups = {};
+ 
+    for (const cookie of groupCookies) {
+        groups[cookie[0]] = JSON.parse(cookie[1]);
+    }
+ 
+    return groups;
+ }
